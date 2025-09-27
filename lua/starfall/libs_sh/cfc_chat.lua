8f0300e9207bb59c612859dfccdfa75ab38e6a33
banoargeek <thatguymajor@protonmail.com>
2025-09-27 17:53:30 -0600

    Fix ChatText hook permissions

--- lua/starfall/libs_sh/cfc_chat.lua
+++ lua/starfall/libs_sh/cfc_chat.lua
@@ -1,7 +1,8 @@
 local registerprivilege = SF.Permissions.registerPrivilege
 local haspermission = SF.Permissions.hasAccess
 
-registerprivilege( "chattext", "ChatText", "Allows the user to see your chat prints.", { client = { default = 1 } } )
+registerprivilege( "chat.text", "Chat", "Allows users to see ALL of your chat prints.", { client = { default = 1 } } )
+registerprivilege( "chat.hide", "Chat", "Allows users to hide chat prints.", { client = { default = 1 } } )
 
 -- Requires the 'ChatText' permission.
 -- NOTE: Does not apply to player chat messages, see http://wiki.facepunch.com/gmod/GM:ChatText
@@ -12,8 +13,12 @@ registerprivilege( "chattext", "ChatText", "Allows the user to see your chat pri
 -- @param string name Name of the player
 -- @param string text Content of the message
 -- @param string type Chat filter type (See http://wiki.facepunch.com/gmod/GM:ChatText).
-SF.hookAdd( "ChatText", "chattext", nil, function( instance )
-    if instance.player == SF.Superuser or haspermission( instance, nil, "chattext" ) then return true end
+SF.hookAdd( "ChatText", "chattext", function( instance, ... )
+    if instance.player == SF.Superuser or haspermission( instance, nil, "chat.text" ) then return true, { ... } end
+    return false
+end,
+function( instance, ret )
+    if haspermission( instance, nil, "chat.hide" ) and ret then return true end
 end )
 
 return function() end
