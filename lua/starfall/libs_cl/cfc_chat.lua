019b1e15c93a6646bebde051fbbd4fdec5afb625
banoargeek <thatguymajor@protonmail.com>
2025-09-29 18:58:01 -0600

    Update cfc_chat.lua
    Cleanup, added `OnChatAddText` to account for missing chat messages.

--- lua/starfall/libs_cl/cfc_chat.lua
+++ lua/starfall/libs_cl/cfc_chat.lua
@@ -1,10 +1,12 @@
 local registerprivilege = SF.Permissions.registerPrivilege
 local haspermission = SF.Permissions.hasAccess
 
-registerprivilege( "chat.text", "Chat", "Allows users to see ALL of your chat prints.", { client = { default = 1 } } )
+registerprivilege( "chat.text", "Chat", "Allows users to see your chat prints.", { client = { default = 1 } } )
 registerprivilege( "chat.hide", "Chat", "Allows users to hide chat prints.", { client = { default = 1 } } )
+registerprivilege( "chat.onchataddtext", "Chat", "Allows users to see your chat prints (chat.addText).", { client = { default = 1 } } )
 
--- Requires the 'ChatText' permission.
+-- Requires the 'chat.text' permission.
+-- Optionally requires the 'chat.hide' permission for hiding messages.
 -- NOTE: Does not apply to player chat messages, see http://wiki.facepunch.com/gmod/GM:ChatText
 -- @name ChatText
 -- @class hook
@@ -19,6 +21,38 @@ SF.hookAdd( "ChatText", "chattext", function( instance, ... )
 end,
 function( instance, ret )
     if ret[1] and haspermission( instance, nil, "chat.hide" ) and ret[2] == true then return true end
+end )
+
+chat._AddText = chat._AddText or chat.AddText
+local oldChatAddText = chat._AddText
+
+chat.AddText = function( ... )
+    print( "[Detour] chat.AddText called", ... )
+    hook.Run( "OnChatAddText", ... )
+
+    return oldChatAddText( ... )
+end
+
+-- Requires the 'chat.onChatAddText' permission.
+-- NOTE: Attempting to print from the hook will result in a stack overflow!
+-- @name onChatAddText
+-- @class hook
+-- @client
+-- @param ... vararg A sequence of arguments passed to chat.AddText.
+--   These can be:
+--   • Color objects
+--   • Player entities
+--   • Strings
+SF.hookAdd( "OnChatAddText", nil, function( instance, ... )
+    local wrapObject = instance.WrapObject
+    local args = { ... }
+
+    for i, arg in ipairs( args ) do
+        args[i] = wrapObject( arg )
+    end
+
+    if instance.player == SF.Superuser or haspermission( instance, nil, "chat.onchataddtext" ) then return true, args end
+    return false
 end )
 
 return function() end
